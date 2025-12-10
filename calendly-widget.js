de8d9323dd74a6a964d91b93fccf3557e5de03d0
duncan-kimputing <duncan@kimputing.com>
2025-12-10 14:48:30 -0600

    Update calendly-widget.js

--- calendly-widget.js
+++ calendly-widget.js
@@ -167,11 +167,11 @@ class CalendlyWidget extends HTMLElement {
       @media (max-width: 700px) {
         .calendly-container {
           border-radius: 8px;
-          min-height: 1200px;
+          min-height: 1500px;
         }
 
         .calendly-iframe {
-          min-height: 1200px;
+          min-height: 1500px;
         }
       }
     `;
